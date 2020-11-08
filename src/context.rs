9fa2985aca310918a5484454e7879443a9ae12a7
cpowicki <chpowicki@gmail.com>
2020-11-08 13:32:40 -0800

    add api mod

--- src/context.rs
+++ src/context.rs
@@ -1,12 +1,26 @@
+use crate::actor::Actor;
+use std::collections::HashMap;
 
-pub struct Actor {
-  id : usize
+pub struct Context {
+    actors: HashMap<u32, Actor>,
 }
 
-impl Actor {
-  fn new(id : usize) -> Self {
-    Actor {
-      id
+impl Context {
+    pub fn new() -> Context {
+        let actors: HashMap<u32, Actor> = HashMap::new();
+        Context { actors }
     }
-  }
+
+    pub fn add_actor(&mut self) {
+        let actor = Actor::new(1);
+        self.actors.insert(actor.get_id(), actor);
+    }
+
+    pub fn get_actor(&self, id: u32) -> Option<&Actor> {
+        self.actors.get(&id)
+    }
+
+    pub fn get_actors(&self) -> Vec<&Actor> {
+        self.actors.values().clone().collect()
+    }
 }
--- src/main.rs
+++ src/main.rs
@@ -1,16 +1,27 @@
+use tokio::sync::Mutex;
 use warp::Filter;
+
+use std::{convert::Infallible, sync::Arc};
+
+mod actor;
+mod context;
+
+type AppContext = Arc<Mutex<context::Context>>;
+
+pub fn init() -> AppContext {
+    Arc::new(Mutex::new(context::Context::new()))
+}
 
 #[tokio::main]
 async fn main() {
-    let hello = warp::path!("hello" / String).map(|name| format!("Hello, {}!", name));
+    let context: AppContext = init();
+    warp::serve(actor::api::routes(context))
+        .run(([127, 0, 0, 1], 3030))
+        .await;
+}
 
-    let (_, server) =
-        warp::serve(hello).bind_with_graceful_shutdown(([127, 0, 0, 1], 3030), async {
-            if let Err(e) = tokio::signal::ctrl_c().await {
-                println!("Error during shutdown {:?}", e);
-            }
-            println!("Shutting down...");
-        });
-
-    tokio::task::spawn(server).await;
+pub fn with_context(
+    context: AppContext,
+) -> impl Filter<Extract = (AppContext,), Error = Infallible> + Clone {
+    warp::any().map(move || context.clone())
 }
